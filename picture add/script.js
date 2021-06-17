cbdf9975dee0794b7337b17936a5d13809a6655c
Alice-63 <alicetindag84@gmail.com>
2021-06-17 11:53:32 +0200

    Picture laden

--- picture add/script.js
+++ picture add/script.js
@@ -1,10 +1,33 @@
 var input=document.getElementById("link");
 var aside=document.getElementById("aside");
-
+var container= document.getElementById("container")
+var span=document.getElementById("span")
 
 function add()
 {
   var img=document.createElement("img");
+   img.src=input.value;
+  img.onclick=function()
+  {
+    imgClick(img.src);
+    
+  }
+
+
+
+
   aside.appendChild(img);
-  img.src=input.value;
+  input.value="";
+}
+
+function imgClick(x)
+{
+
+  span.style.display="block"
+
+  setTimeout(function(){container.style.backgroundImage="url("+x+")";
+  span.style.display="none"},1000)
+  
+
+
 }
