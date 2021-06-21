7d6b698c8b1cd93abe3888aa7b5bf4a4355ad2b6
Alice-63 <alicetindag84@gmail.com>
2021-06-21 18:38:34 +0200

    created Type new item

---
+++ Type new item/script.js
@@ -0,0 +1,37 @@
+
+var erg=document.getElementById("erg");
+var zahl=0;
+var i=document.getElementById("i");
+
+i.addEventListener('click', add)
+
+var h2=document.createElement("h2");
+
+function add()
+{
+    var erg=document.getElementById("erg");
+    var input=document.getElementById("inp").value;
+   if(input=="")
+    {
+        alert("Tüm alanlari doldurun")
+    }
+else
+    {
+    
+        var number=Math.floor(Math.random()*1000000)
+    var h2=document.createElement("h2");
+    h2.className="h2";
+    h2.appendChild(document.createTextNode(input))
+    erg.appendChild(h2);
+    h2.style.backgroundColor="#"+number;
+    h2.style.color="#"+(number+511111);
+    document.getElementById("inp").value="";
+    zahl++;
+    add2 (zahl);
+    console.log(number)
+    }
+}
+function add2(a)
+{
+    
+}
