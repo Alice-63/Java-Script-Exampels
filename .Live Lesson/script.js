6d50a97eb9362da765a4d1ce37cd6d1a3950c94d
Alice-63 <alicetindag84@gmail.com>
2021-06-29 11:17:24 +0200

    changes

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -1,4 +1,11 @@
+var d=new Date;
+
+console.log(d.getDate());
+console.log(d.getDay());
+console.log(d.getFullYear());
+console.log(d.getHours());
+console.log(d.getMinutes());
 
 
-
-
+document.getElementById("box").innerHTML=d.getMonth()+" "+d.getDate()+" "+d.getFullYear()+"<br>"
++d.getHours()+" "+d.getMinutes()+" "+d.getSeconds();
