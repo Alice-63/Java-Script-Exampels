2e6fd4bdd2d9a7e1cf38d14f47b085a5f6cb4ac9
Alice-63 <alicetindag84@gmail.com>
2021-06-29 12:03:39 +0200

    add

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -1,21 +1,10 @@
-var d=new Date;
-
-console.log(d.getDate());
-console.log(d.getDay());
-console.log(d.getFullYear());
-console.log(d.getHours());
-console.log(d.getMinutes());
-
-setInterval(myFunction,1000)
+var d=new Date("02,05,1984")
 
 
-function myFunction() {
-    setInterval(time, 1000);
-  }
+
+var x=new Date();
 
 
-function time()
-{
-document.getElementById("box").innerHTML=d.getMonth()+" "+d.getDate()+" "+d.getFullYear()+"<br>"
-+d.getHours()+" "+d.getMinutes()+" "+d.getSeconds();
-}
+var c=x.getFullYear()-d.getFullYear();
+
+console.log(d);
