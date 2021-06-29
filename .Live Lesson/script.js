44e2e3a728579d8252adcfc1eaa4162ed32e9328
Alice-63 <alicetindag84@gmail.com>
2021-06-29 11:24:40 +0200

    Update script.js

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -6,6 +6,16 @@ console.log(d.getFullYear());
 console.log(d.getHours());
 console.log(d.getMinutes());
 
+setInterval(myFunction,1000)
 
+
+function myFunction() {
+    setInterval(time, 1000);
+  }
+
+
+function time()
+{
 document.getElementById("box").innerHTML=d.getMonth()+" "+d.getDate()+" "+d.getFullYear()+"<br>"
 +d.getHours()+" "+d.getMinutes()+" "+d.getSeconds();
+}
