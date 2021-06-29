5dee805ed5e45594c60ba3543e20f8a2c5da31ec
Alice-63 <alicetindag84@gmail.com>
2021-06-29 20:56:28 +0200

    add

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -1,10 +1,23 @@
-var d=new Date("02,05,1984")
+var allA=document.getElementsByTagName("a");
+var a=0;
 
+function show()
+{
+    a++;
+    if(a==1)
+    {
+    for(i=0 ;i < allA.length ; i++)
+    {
+        allA[i].style.display="block";
+    }
+    }
+    if(a==2)
+    {
+        for(i=0 ;i < allA.length ; i++)
+    {
+        allA[i].style.display="none";
+        a=0;
+    }
 
-
-var x=new Date();
-
-
-var c=x.getFullYear()-d.getFullYear();
-
-console.log(d);
+    }
+}
