a1311d98c645be2334c95c0409c6332f2d4f88ec
Alice-63 <alicetindag84@gmail.com>
2021-07-09 15:50:04 +0200

    add

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -25,7 +25,9 @@ for(let x=0; x<val.length;x++)
 
 function add(a)
 {
-    a.innerHTML="HAllo";
-    a.onclick=function(){a.innerHTML="Klick";a.onclick=function(){add(a)}}
+   var box=document.getElementById("box");
+   box.style.display="none";
+    // a.innerHTML="Hallo";
+    a.onclick=function(){a.innerHTML="Klick"; box.style.display="block";box.style.width="350px";a.onclick=function(){add(a)}}
   
 }
