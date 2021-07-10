84843e750e170e096da8e39b607a994eacf684e0
Alice-63 <alicetindag84@gmail.com>
2021-07-10 09:50:53 +0200

    Update script.js

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -1,49 +1,10 @@
 
 
 
-function Auto(a,b,c)
+function Person()
 {
-    this.name=a;
-    this.nachname=b;
-    this.fullName=c;
+    name:"ALi";
+    surname:"Cetin";
+    fullName: function(){this.name+" "+this.surname}
 }
-
-
-const val={
-    name:"ALi",
-    nachname:"Cetindag"
-}
-
-val.full="ALi Cetindag";
-val.job="Web Etwickler";
-delete val;
-var box=document.getElementById("box");
-
-var myArray=Object.values(val);
-console.log(myArray)
-
-for(i=0; i<myArray.length; i++)
-{
-    box.innerHTML+=myArray[i]+"<br>";
-}
-
-// function add(a)
-// {
-//    
-//    box.style.display="none";
-//     // a.innerHTML="Hallo";
-//     a.onclick=function(){a.innerHTML="Klick"; box.style.display="block";box.style.width="350px";a.onclick=function(){add(a)}}
-  
-// }
-
-function add2(a)
-{
-    let input=document.getElementById("input").value;
-    
-
-    var n=input;
-    n=n.toUpperCase();
-
-    box.innerHTML+=n+"<br>";
-    console.log(n)
-}
+console.log(Person)
