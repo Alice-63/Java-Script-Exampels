acaa67c96f751a4848670db69cc73d736b63c6c3
Alice-63 <alicetindag84@gmail.com>
2021-07-14 12:41:08 +0200

    Update script.js

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -13,8 +13,6 @@ surname:["cetin", "dag","öz","onur","Can"],
 age:[37,25,35,62,12]
 }
 
-console.log(Object(person1))
-
 for(x in person1)
 {
     let div=document.createElement("div");
@@ -23,15 +21,12 @@ for(x in person1)
     div2.innerHTML=x;
     div.appendChild(div2);
 
-    let arr=person1[x];
-
-    for(y of arr )
-
+    let arr=person1[x]
+    for(y of arr)
     {
         let div3=document.createElement("div");
         div3.innerHTML=y;
-        div.appendChild(div3)
+        div.appendChild(div3);
     }
-
     document.body.appendChild(div);
 }
