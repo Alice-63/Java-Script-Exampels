fcdf7dff28413c99543b3b14cd078e043827c522
Alice-63 <alicetindag84@gmail.com>
2021-07-14 11:06:13 +0200

    Update script.js

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -4,30 +4,18 @@ var inpSurname=document.getElementById("input2").value;
 var inpAge=document.getElementById("input3").value;
 
 
-function Person(name,surname,age)
-{
-    this.Name=name;
-    this.Surname=surname;
-    this.Age=age
-}
 
-function add2()
 
+
+
+const person1=["ALi", "Cetindag",37]
+
+for(x of person1)
 {
 
-    var box=document.getElementById("box");
-    var inpName=document.getElementById("input1").value;
-    var inpSurname=document.getElementById("input2").value;
-    var inpAge=document.getElementById("input3").value;
-
-const person1=new Person(inpName,inpSurname,inpAge)
-
-for(x in person1)
-{
-
-    box.innerHTML+=x+" : "+person1[x]+"<br>"
+    box.innerHTML+=x+"<br>"
 
 }
 
-}
+
 
