b2cb18e269c7fb65fb12e7cee5804169355f38e7
Alice-63 <alicetindag84@gmail.com>
2021-07-15 18:25:39 +0200

    Update script.js

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -2,19 +2,6 @@
 $(function()
 {
 
-    $("button").click(function () { 
-        const box=$("#box");
-
-        const div=document.createElement("div");
-        div.innerHTML=$("input").val();
-        box.before(div);
-        $("button:last-child").remove();
-    });
-
-
-
-
-
 
 
 
