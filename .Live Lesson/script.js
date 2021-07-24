45d2a3c72e66ee850f62c54e3219aaae2afa01b7
Alice-63 <alicetindag84@gmail.com>
2021-07-24 12:07:17 +0200

    add

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -1,36 +1,4 @@
-
-
-function kardeslereYaziYaz(e)
-{
-    let onceki=e.previousElementSibling;
-    let sonraki=e.nextElementSibling;
-
-    while(onceki)
-    {
-        onceki.innerHTML="Merhaba"
-        onceki=onceki.previousElementSibling;
-        
-    }
-
-    while(sonraki)
-
-    { 
-        sonraki.innerHTML="Hallo"
-        sonraki=sonraki.nextElementSibling;
-    }
-
-}
-
-kardeslereYaziYaz(document.getElementById("test"))
-
-
-// $(document).ready(function () {
-    
-// $("#test").prevAll().text("Merhaba");
-// $("#test").nextAll().text("Hallo")
-
-// });
-
+AOS.init();
 
 
 
