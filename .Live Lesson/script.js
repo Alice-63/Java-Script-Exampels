cdfb0fe6159fc6ca108dc54a5a1ecbe77922fec6
Alice-63 <alicetindag84@gmail.com>
2021-07-28 11:13:37 +0200

    add

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -2,16 +2,23 @@
 
 $(document).ready(function(){
 
+ $("#animate").click(function(){
+
+    let box=$("#box1");
+
+    box.animate({
+        left:"700px"
+    },2000,function next(){
+        let box=document.getElementById("box1");
+        
+        box.style.left="700px"
+     }
+    )
+    
+
+ })
+  
  
-    $("#show").click(function(){
-        $("p").slideDown(1000)
-    })
-    $("#hide").click(function(){
-        $("p").slideUp(1000)
-    })
-    $("#toggle").click(function(){
-        $("p").slideToggle(1000)
-    })
 
 
 })
