72a2f8ec9b088f8c02e2676f642ca9eb0ed6c1ee
Alice-63 <alicetindag84@gmail.com>
2021-07-28 09:26:17 +0200

    add

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -1,5 +1,15 @@
 
 
+$(document).ready(function(){
 
+    $("#control"). change(function(){
+        let val=$(this).val();
+        $("#box").append(val)
+        $(this).val(" ")
+    })
+
+
+
+})
 
    
