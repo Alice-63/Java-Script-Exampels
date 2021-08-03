8afda56b99c960a5d6ad0bac84aacef68b9fa61f
Alice-63 <alicetindag84@gmail.com>
2021-08-04 00:38:34 +0200

    add

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -5,7 +5,37 @@ new Vue({
     el: "#app",
     data:{
         title:"Test Baslik",
-        button:"Click"
+        button:"Click",
+        paragraph:"lorem impsum ",
+        link:"https://www.google.com",
+        details:"<b> Bu Bir haber detayidir...</b>",
+        id:123,
+    }
+})
+
+new Vue({
+
+
+    el:"#app2",
+    data:{
+        sayi:0,
+    },
+    methods:
+    {
+        tikla()
+        {
+            this.sayi++
+        },
+
+        tik(e)
+        {
+            if(e.keyCode==13)
+           {
+               this.sayi+=parseInt(e.target.value)
+                e.target.value="";
+            }
+            
+        }
     }
 })
 
