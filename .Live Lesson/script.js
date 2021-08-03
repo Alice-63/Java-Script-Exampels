f1ba90322bdf0f3c5ec59f6b3389cca8b2310cf2
Alice-63 <alicetindag84@gmail.com>
2021-08-03 18:18:39 +0200

    Update script.js

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -2,22 +2,6 @@
 
 $(document).ready(function(){
 
- $("#animate").click(function(){
-
-    let box=$("#box1");
-
-    box.animate({
-        left:"700px"
-    },2000,function next(){
-        let box=document.getElementById("box1");
-        
-        box.style.left="700px"
-     }
-    )
-    
-
- })
-  
  
 
 
