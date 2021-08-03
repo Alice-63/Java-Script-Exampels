0e8017406f836a258cde5a7c561d783c1e259ca3
Alice-63 <alicetindag84@gmail.com>
2021-08-03 20:20:17 +0200

    add

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -6,5 +6,23 @@ function update()
 {
     let number=textarea.value.length;
     span.innerHTML=number+ " /100"
+
+
+    if(number==100)
+    {
+        span.style.color="red"
+    }
+    else if(number>=90)
+    {
+        span.style.color="#da2d2d"
+    }
+    else if(number>=70)
+    {
+        span.style.color="#b33737"
+    }
+    else
+    {
+        span.style.color="black"
+    }
 }
 
