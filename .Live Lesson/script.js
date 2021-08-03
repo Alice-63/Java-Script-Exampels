186a1afe10905691a7da04506c093177672c4e41
Alice-63 <alicetindag84@gmail.com>
2021-08-03 18:52:53 +0200

    Update script.js

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -11,4 +11,10 @@ var box=document.getElementById("box1");
 
 box.innerHTML=box.innerHTML.replace("ali cetindag","ahmet zambak")
 
+
+const val=0;
+
+console.log(val)
    
+val=15;
+console.log(val)
