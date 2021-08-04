1a556395d07893c278d46efd4f58cb932dcf65b0
Alice-63 <alicetindag84@gmail.com>
2021-08-04 21:06:42 +0200

    add

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -7,15 +7,37 @@ var app=new Vue({
 
     el:"#app",
     data:{
-        text:"Ali"
+        character:"+"
     },
     methods:
     {
-        add(e){
-            app.text=e
+        cal(e){
+            app.character=e
         },
         
     }
 
 })
 
+
+
+function calculate()
+{
+
+
+const spn=document.getElementById("spn");
+const result=document.getElementById("result");
+var inp1=document.getElementById("inp1").value;
+var inp2=document.getElementById("inp2").value;
+
+    if(spn.innerHTML== "+")
+        {
+        console.log(inp1)
+        console.log(inp2)
+        result.innerHTML=parseInt(inp1)+parseInt(inp2);
+        }
+    else if(spn.innerHTML=="-")
+        {
+        result.innerHTML=parseInt(inp1)-parseInt(inp2);
+        }
+}
