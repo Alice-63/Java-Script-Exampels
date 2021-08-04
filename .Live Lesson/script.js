e9a6fc97986bb90588a01a929ea133f460d0dcf5
Alice-63 <alicetindag84@gmail.com>
2021-08-04 21:14:47 +0200

    add

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -29,15 +29,22 @@ const spn=document.getElementById("spn");
 const result=document.getElementById("result");
 var inp1=document.getElementById("inp1").value;
 var inp2=document.getElementById("inp2").value;
+var war=document.getElementById("warning");
 
-    if(spn.innerHTML== "+")
+    if(inp1=="" || inp2=="")
+    {
+        war.innerHTML="Please fill in all fields!";
+        war.style.color="red";
+        return
+    }
+    else if(spn.innerHTML== "+")
         {
-        console.log(inp1)
-        console.log(inp2)
-        result.innerHTML=parseInt(inp1)+parseInt(inp2);
+            result.innerHTML=parseInt(inp1)+parseInt(inp2);
+            war.innerHTML=""
         }
     else if(spn.innerHTML=="-")
         {
         result.innerHTML=parseInt(inp1)-parseInt(inp2);
+        war.innerHTML=""
         }
 }
