0e6c3deb47724393fba6bda0385fcde89b5cbad2
Alice-63 <alicetindag84@gmail.com>
2021-08-05 19:50:44 +0200

    add

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -5,28 +5,11 @@ const app=new Vue
 ({
     el:"#app",
     data:{
-        sayi:0,
-        number:0
+        text:""
     },
-    methods:{
-        art(){
-            this.sayi++
-        },
-        azt(){
-            this.sayi--
-        },
-        sayiWriter(){
-            console.log("Hallo Methods")
-            return this.sayi>=18 ? "Resittir":"Degildir"
-        }
-    },
-    computed:{
-        sayiWriterCom(){
-            console.log("Hallo Computed")
-            return this.sayi>=18 ? "Resittir":"Degildir"
-        }
-    }
-
+    
+   
+    
 
 })
 
