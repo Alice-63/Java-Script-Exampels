9b2ef00037babacfa64ab446156f18a6dbc9c5d2
Alice-63 <alicetindag84@gmail.com>
2021-09-16 21:08:06 +0200

    add

---
+++ AutoComplet/script.js
@@ -0,0 +1,21 @@
+
+const produktnamen = 
+[
+    "Tasche",
+    "Kühlschrank",
+    "Arbeitshose",
+    "Tisch",
+    "Kühltasche",
+    "Kühlbox",
+    "Jacke",
+    "Kühltruhe",
+    "Hemd",
+    "Kopftuch",
+]
+
+function tip(el)
+{
+    let s = el.value; // Kühl
+
+    
+}
